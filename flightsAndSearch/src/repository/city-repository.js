d1f00c1fb611c96d3e14b41b97d5b4e28255e742
Ankush2107 <ankush.kumar210701@gmail.com>
2024-07-14 11:22:15 +0530

    added filters to get all cities

--- flightsAndSearch/src/repository/city-repository.js
+++ flightsAndSearch/src/repository/city-repository.js
@@ -1,3 +1,4 @@
+const { Op } = require("sequelize");
 const { city } = require("../models/index");
 
 class CityRepository {
@@ -58,8 +59,18 @@ class CityRepository {
         }
     }
 
-    async getAllCities() {
+    async getAllCities(filter) {     // filter can be empty also.
         try {
+            if(filter.name) {
+                const cities = await city.findAll({
+                    where: {
+                        name: {
+                            [Op.startsWith]: filter.name
+                        }
+                    }
+                })
+                return cities;
+            }
             const cities = await city.findAll();
             return cities;
         } catch (error) {
