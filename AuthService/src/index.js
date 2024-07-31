2daaaec11d3fd56cbea77651fedd066be8878704
Ankush2107 <ankush.kumar210701@gmail.com>
2024-07-31 10:44:17 +0530

    added bcrypt

--- AuthService/src/index.js
+++ AuthService/src/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
-const { PORT } = require("./config/serverconfig")
+const bodyParser = require('body-parser');
+
+const { PORT } = require("./config/serverconfig");
+const apiRoutes = require("./routes/index");
+
 const app = express();
 
-
 const prepareAndStartServer = () => {
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+    app.use('/api', apiRoutes);
+
     app.listen(PORT, () => {
         console.log(`Server started on PORT: ${PORT}`);
     })
